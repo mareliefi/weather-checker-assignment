c09aa351f93256a5ec5bff97ec81d64d8e8cab42
Marli Coetzee <mareliefies@gmail.com>
2019-06-07 15:43:55 +0200

    Error handling included

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
-$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+$(function () { 
   $.get("http://api.openweathermap.org/data/2.5/weather?q=Pretoria,za&units=metric&APPID=ad072cb9c0700a66a327e65573d6cee8", function(response) {
-    var weatherresult = response;
-    var name = "City: " + weatherresult.name;
-    var temp = "Temperature: " + weatherresult.main.temp + "°C";
-    var tempMax = "Maxmimum Temperature: " + weatherresult.main.temp_max + "°C";
-    var tempMin = "Minimum Temperature: " + weatherresult.main.temp_min + "°C";
-    var pressure = "Pressure: " + weatherresult.main.pressure + " Pa";
-    var humidity = "Humidity: " + weatherresult.main.humidity + " g/m3";
-    var weatherdescription = "Weather description: " + weatherresult.weather[0].main;
-    console.log(weatherresult);
+    var weatherResult = response;
+    var name = "City: " + weatherResult.name;
+    var temp = "Temperature: " + weatherResult.main.temp + "°C";
+    var tempMax = "Maxmimum Temperature: " + weatherResult.main.temp_max + "°C";
+    var tempMin = "Minimum Temperature: " + weatherResult.main.temp_min + "°C";
+    var pressure = "Pressure: " + weatherResult.main.pressure + " Pa";
+    var humidity = "Humidity: " + weatherResult.main.humidity + " g/m3";
+    
+    if(weatherResult.weather.length > 0) {
+      var weatherDescription = "Weather circumstances are: " + weatherResult.weather[0].main;
+    } else {
+      var weatherDescription = "No weather description avaiable";
+    }
+
+    document.getElementById("weatherHeading").innerHTML = "Current Weather:"
     document.getElementById("name").innerHTML = name;
     document.getElementById("temp").innerHTML = temp;
     document.getElementById("tempMax").innerHTML = tempMax;
     document.getElementById("tempMin").innerHTML = tempMin;
     document.getElementById("pressure").innerHTML = pressure;
-    document.getElementById("humidity").innerHTML = humidity;  
-    document.getElementById("weatherdescription").innerHTML = weatherdescription;
+    document.getElementById("humidity").innerHTML = humidity;
+    document.getElementById("weatherDescription").innerHTML = weatherDescription;  
   })
-true;
-});
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    document.getElementById("weatherHeading").innerHTML = "Error:" + "</br>" + errorThrown;
+  });
+});    
+
